b547194eb0f0e9dafc96fa8df9ba27bb3d438550
ZaccurSk <SkhumbuzoRakgoale@outlook.com>
2022-12-08 15:47:46 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - This uses program prints exactly the string
- * "Programming is like building a multilingual puzzle
+ * main - Prints a text
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
- 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
