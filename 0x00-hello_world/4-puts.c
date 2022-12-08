27cd0dfa7acb7a21d8a4c153e201959d219a0644
ZaccurSk <SkhumbuzoRakgoale@outlook.com>
2022-12-08 14:40:48 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  *  * main - This uses program prints exactly the string
  *   * "Programming is like building a multilingual puzzle
- *    * followed by a new line.
+ *    * 
  *     * Return: always 0 (Success)
  *      */
 int main(void)
