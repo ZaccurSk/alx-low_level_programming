6b2fef5ebe2629e0d270d206d385f0d353a9373f
ZaccurSk <SkhumbuzoRakgoale@outlook.com>
2023-01-03 16:49:48 -0800

    Array

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,14 +5,19 @@
  * @a: double pointer
  * Return: 0
  */
+
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, j;
+	unsigned int i, m = 0;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 64; i++)
 	{
-		for (j = 0; j < 8; j++)
-			_putchar(a[i][j]);
-		        _putchar('\n');
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
 	}
+	_putchar('\n');
 }
