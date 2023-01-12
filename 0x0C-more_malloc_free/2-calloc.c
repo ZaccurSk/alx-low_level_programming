6f0b7e19433ff53fe19a499586dabf0d1445ab9d
ZaccurSk <SkhumbuzoRakgoale@outlook.com>
2023-01-12 13:17:05 -0800

    More Malloc Free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,23 +12,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i = 0, l = 0;
 	char *p;
+	unsigned int a;
+	void *tmp;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-
-	l = nmemb * size;
-	p = malloc(l);
-
-	if (p == NULL)
-		return (NULL);
-
-	while (i < l)
+	p = malloc(nmemb * size);
+	tmp = p;
+	if (p == 0)
 	{
-		p[i] = 0;
-		i++;
+		return (NULL);
 	}
-	
-	return (p);
+	for (a = 0; a < (nmemb * size); a++)
+		p[a] = 0;
+	return (tmp);
 }
