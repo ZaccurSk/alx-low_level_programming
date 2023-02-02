637ca9fcf7f3fc093260a4a9177c0868b352477e
ZaccurSk <SkhumbuzoRakgoale@outlook.com>
2023-02-02 00:46:56 -0800

    Preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ *  main - check the code
+ *
+ *  Return: Always 0.
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts(__BASE_FILE__);
+	return (0);
+}
